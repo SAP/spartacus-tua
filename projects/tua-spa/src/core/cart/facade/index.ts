c63c85b2847e4d7152f5c5b338e87c702917bd7e
C5294972 <sivaprasad.kare@sap.com>
2020-10-05 04:41:16 +0100

    old version files deleted

--- projects/tua-spa/src/core/cart/facade/index.ts
+++
@@ -1,9 +0,0 @@
-/*
- * SPDX-FileCopyrightText: 2020 SAP SE or an SAP affiliate company <deborah.cholmeley-jones@sap.com>
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-export * from './tma-active-cart.service';
-export * from './tma-cart.service';
-export * from './tma-cart-price.service';
-export * from './tma-multi-cart.service';
--- projects/tua-spa/src/core/cart/index.ts
+++
@@ -1,8 +0,0 @@
-/*
- * SPDX-FileCopyrightText: 2020 SAP SE or an SAP affiliate company <deborah.cholmeley-jones@sap.com>
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-export * from './connectors/index';
-export * from './facade/index';
-export * from './store/index';
