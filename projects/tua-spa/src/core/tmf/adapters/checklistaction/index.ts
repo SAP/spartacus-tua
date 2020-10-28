4abc49925e1a652ed7060f834fdeca9b9d1f0591
C5294972 <sivaprasad.kare@sap.com>
2020-10-28 11:53:09 +0100

    removed old version code

--- projects/tua-spa/src/core/tmf/adapters/checklistaction/index.ts
+++
@@ -1,8 +0,0 @@
-/*
- * SPDX-FileCopyrightText: 2020 SAP SE or an SAP affiliate company <deborah.cholmeley-jones@sap.com>
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-export * from './tma-tmf-checklist-action.module';
-export * from './converters/index';
-export * from './tma-tmf-checklist-action.adapter';
